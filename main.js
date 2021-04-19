01c5a94c6e546453ba2b9c987047d17fe70a1ba5
nicocalezich <ncalezich@gmail.com>
2021-04-19 15:02:20 -0300

    full app loaded

---
+++ main.js
@@ -0,0 +1,46 @@
+new Vue({
+    el: '#app',
+    data: {
+        red: document.getElementById('red'),
+        yellow: document.getElementById('yellow'),
+        green: document.getElementById('green'),
+        redNGrenTime: 10000,
+        yellowTime: 3000,
+        isOn: false,
+        btntext: "Turn on",
+        btn: document.getElementById('btn')
+    },
+
+    methods: {
+        iniciar(){
+            setTimeout(this.redToYellow, this.redNGrenTime);
+            this.btntext = "Iniciado"
+            btn.disabled = true
+          },
+          
+          redToYellow(){
+            red.style.backgroundColor = 'white'
+            yellow.style.backgroundColor = 'yellow'
+            setTimeout(this.yellowToGreen, this.yellowTime);
+          },
+          
+          yellowToGreen(){
+            yellow.style.backgroundColor = 'white'
+            green.style.backgroundColor = 'green'
+            setTimeout(this.greenToYellow, this.redNGrenTime);
+          },
+          
+          greenToYellow(){
+            yellow.style.backgroundColor = 'yellow'
+            green.style.backgroundColor = 'white'
+            setTimeout(this.yellowToRed, this.yellowTime);
+          },
+          
+          yellowToRed(){
+            yellow.style.backgroundColor = 'white'
+            red.style.backgroundColor = 'red'
+            this.iniciar()
+          }
+          
+    }
+});
